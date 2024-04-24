ad4e4e6f7dcea78a84d6fc2014a0d9f656c3e381
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2024-04-24 11:16:47 +0200

    add padding and spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,24 +4,26 @@ import gitHub from '../assets/images/github.svg';
 
 const Footer = () => {
   return (
-    <footer className='padding-x  border-t-2 '>
-      <Link to='/' className=' '>
-        <h1 className='dark:text-white text-3xl font-bold font-palanquin max-md: pt-8 pb-4'>
-          Blog
-          <span className='primary-color '>API</span>
-        </h1>
-      </Link>
-      <div className='flex justify-center gap-10 pb-14 '>
-        {footerText.map((el) => (
-          <div>
-            <h3 className='dark:text-white text-xl font-bold pb-2'>
-              {el.title}
-            </h3>
-            {el.links.map((link) => (
-              <p className='info-color py-1'>{link}</p>
-            ))}
-          </div>
-        ))}
+    <footer className='padding-x border-t-2 '>
+      <div className='max-container'>
+        <Link to='/' className=' '>
+          <h1 className='dark:text-white text-3xl font-bold font-palanquin max-md: pt-8 pb-4'>
+            Blog
+            <span className='primary-color '>API</span>
+          </h1>
+        </Link>
+        <div className='flex justify-center gap-10 pb-14 '>
+          {footerText.map((el) => (
+            <div>
+              <h3 className='dark:text-white text-xl font-bold pb-2'>
+                {el.title}
+              </h3>
+              {el.links.map((link) => (
+                <p className='info-color py-1'>{link}</p>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </footer>
   );

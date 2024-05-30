4a2fabe2b49146d2ab8358d4cce6501ad9fbd058
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2024-05-30 14:27:06 +0200

    posts are deletable

--- src/components/EditCards.jsx
+++ src/components/EditCards.jsx
@@ -1,8 +1,33 @@
 import { Link } from 'react-router-dom';
 import { convertString } from '../hooks/helpers';
 import { TrashIcon, PencilIcon, CheckIcon } from '@heroicons/react/24/solid';
-
+import { usePosts } from '../context/postsContext';
+import { useSession } from '../context/sessionContext';
+import axios from 'axios';
 const EditCards = ({ post }) => {
+  const { fetchPosts } = usePosts();
+  const { accessToken } = useSession();
+
+  const deleteProject = async () => {
+    try {
+      const res = await axios.delete(
+        `http://localhost:3000/posts`,
+
+        {
+          data: {
+            postId: post._id,
+          },
+          headers: {
+            authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      console.log(res);
+      fetchPosts();
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <li className='flex surface justify-between items-center px-3 py-4 rounded-lg'>
       <div className='flex gap-5 '>
@@ -22,7 +47,10 @@ const EditCards = ({ post }) => {
         <div className='border-[3px] p-2 rounded-xl border-purple-700 dark:border-purple-300 hover:border-purple-600 dark:hover:border-purple-200  transition-all cursor-pointer'>
           <PencilIcon className='w-5 h-5 primary-color  hover:text-purple-600 dark:hover:text-purple-200 transition-all ' />
         </div>
-        <div className='border-[3px] p-2 rounded-xl border-purple-700 dark:border-purple-300 hover:border-purple-600 dark:hover:border-purple-200  transition-all cursor-pointer'>
+        <div
+          className='border-[3px] p-2 rounded-xl border-purple-700 dark:border-purple-300 hover:border-purple-600 dark:hover:border-purple-200  transition-all cursor-pointer '
+          onClick={deleteProject}
+        >
           <TrashIcon className='w-5 h-5 primary-color  hover:text-purple-600 dark:hover:text-purple-200 transition-all ' />
         </div>
       </div>
